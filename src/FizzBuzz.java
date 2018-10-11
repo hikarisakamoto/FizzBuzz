9eea84704000a68faa6c69dc43ef3e92148c4d62
Hikari Sakamoto <hikarisakamoto@gmail.com>
2018-10-10 21:17:24 -0300

    Update separando a responsabilidade da função de verificar o numero para cada tipo de divisão 3 ou 5

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,11 +5,21 @@ public class FizzBuzz {
 
     protected String numeroFizzBuzz(int numero) {
         String fizzBuzzOuNumero = String.valueOf(numero);
-        // Teste para numero divisível por 3
+
+        fizzBuzzOuNumero = validarFizz(numero, fizzBuzzOuNumero);
+        fizzBuzzOuNumero = validarBuzz(numero, fizzBuzzOuNumero);
+
+        return fizzBuzzOuNumero;
+    }
+
+    private String validarFizz(int numero, String fizzBuzzOuNumero) {
         if (numero % 3 == 0) {
             fizzBuzzOuNumero = fizz;
         }
-        // Teste para numero divisível por 5
+        return fizzBuzzOuNumero;
+    }
+
+    private String validarBuzz(int numero, String fizzBuzzOuNumero) {
         if (numero % 5 == 0) {
             if (fizzBuzzOuNumero.equals(fizz)) {
                 fizzBuzzOuNumero += buzz;
@@ -21,20 +31,21 @@ public class FizzBuzz {
     }
 
     public void gerarListaCompletaAte100() {
-        // Pode se trocar o valor fixo por um parametro do método
-        int numeroFinalDaLista = 100;
-        for (int i = 1; i <= numeroFinalDaLista; i++) {
+        for (int i = 1; i <= 100; i++) {
             System.out.println(numeroFizzBuzz(i));
         }
     }
 
     private String numeroFizzBuzzStage2(int numero) {
         String fizzBuzzOuNumero = String.valueOf(numero);
-        // Teste para numero divisível por 3
-        if (numero % 3 == 0 || fizzBuzzOuNumero.contains("3")) {
-            fizzBuzzOuNumero = fizz;
-        }
-        // Teste para numero divisível por 5
+
+        fizzBuzzOuNumero = validarFizzStage2(numero, fizzBuzzOuNumero);
+        fizzBuzzOuNumero = validarBuzzStage2(numero, fizzBuzzOuNumero);
+
+        return fizzBuzzOuNumero;
+    }
+
+    private String validarBuzzStage2(int numero, String fizzBuzzOuNumero) {
         if (numero % 5 == 0 || fizzBuzzOuNumero.contains("5")) {
             if (fizzBuzzOuNumero.equals(fizz)) {
                 fizzBuzzOuNumero += buzz;
@@ -45,9 +56,14 @@ public class FizzBuzz {
         return fizzBuzzOuNumero;
     }
 
+    private String validarFizzStage2(int numero, String fizzBuzzOuNumero) {
+        if (numero % 3 == 0 || fizzBuzzOuNumero.contains("3")) {
+            fizzBuzzOuNumero = fizz;
+        }
+        return fizzBuzzOuNumero;
+    }
+
     void gerarListaCompletaAte100FizzBuzzStage2() {
-        // Pode se trocar o valor fixo por um parametro do método
-        int numeroFinalDaLista = 100;
         for (int i = 1; i <= 100; i++) {
             System.out.println(numeroFizzBuzzStage2(i));
         }
