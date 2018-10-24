be1c5831ad5d6623b8a2ec774008005210833626
Hikari Sakamoto <hikarisakamoto@gmail.com>
2018-10-24 00:03:04 -0300

    Codificacao para passar o teste de numeros divisiveis por 3

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,10 +2,11 @@ public class FizzBuzz {
     protected String numeroFizzBuzz(int numero) {
         String fizzBuzzOuNumero = String.valueOf(numero);
 
-        if (fizzBuzzOuNumero.equals("1")) {
-            return "1";
-        } else {
-            return "71";
+        if (numero % 3 == 0) {
+            fizzBuzzOuNumero = "Fizz";
         }
+
+
+        return fizzBuzzOuNumero;
     }
 }
