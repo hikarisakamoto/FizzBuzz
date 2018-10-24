f95e91d56adea7ba1e972a0d4d02bae916b62d31
Hikari Sakamoto <hikarisakamoto@gmail.com>
2018-10-24 00:10:23 -0300

    Refatoracao do codigo

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,14 +1,25 @@
 public class FizzBuzz {
+
     private final String fizz = "Fizz";
     private final String buzz = "Buzz";
-
 
     protected String numeroFizzBuzz(int numero) {
         String fizzBuzzOuNumero = String.valueOf(numero);
 
+        fizzBuzzOuNumero = validarFizz(numero, fizzBuzzOuNumero);
+        fizzBuzzOuNumero = validarBuzz(numero, fizzBuzzOuNumero);
+
+        return fizzBuzzOuNumero;
+    }
+
+    private String validarFizz(int numero, String fizzBuzzOuNumero) {
         if (numero % 3 == 0) {
             fizzBuzzOuNumero = fizz;
         }
+        return fizzBuzzOuNumero;
+    }
+
+    private String validarBuzz(int numero, String fizzBuzzOuNumero) {
         if (numero % 5 == 0) {
             if (fizzBuzzOuNumero.equals(fizz)) {
                 fizzBuzzOuNumero += buzz;
@@ -17,5 +28,11 @@ public class FizzBuzz {
             }
         }
         return fizzBuzzOuNumero;
+    }
+
+    public void gerarListaCompletaAte100() {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(numeroFizzBuzz(i));
+        }
     }
 }
