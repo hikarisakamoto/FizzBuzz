802545db23332d22738d9c746b187975f3d6f5df
Hikari Sakamoto <hikarisakamoto@gmail.com>
2018-10-24 00:03:41 -0300

    Refatoracao

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,12 +1,12 @@
 public class FizzBuzz {
+    private final String fizz = "Fizz";
+
     protected String numeroFizzBuzz(int numero) {
         String fizzBuzzOuNumero = String.valueOf(numero);
 
         if (numero % 3 == 0) {
-            fizzBuzzOuNumero = "Fizz";
+            fizzBuzzOuNumero = fizz;
         }
-
-
         return fizzBuzzOuNumero;
     }
 }
